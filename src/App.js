b5b836160fd9f4aaea3b2e1d1b94217b15f359d0
ldail <leondailofficial@gmail.com>
2020-01-08 21:41:37 -0800

    sign up created

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {Switch, Route} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component'
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 import './App.css';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
@@ -20,8 +20,24 @@ class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user});
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapshot => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data()
+            }
+          }, () => {
+            console.log(this.state);
+          })
+        })
+      }
+      else {
+        this.setState({ currentUser: userAuth});
+      }
     })
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,16 +3,47 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDm3JMHGSu8-hpDLE4OSQFgzEnFMcjE5A4",
-    authDomain: "crown-clothing-db-42ce3.firebaseapp.com",
-    databaseURL: "https://crown-clothing-db-42ce3.firebaseio.com",
-    projectId: "crown-clothing-db-42ce3",
-    storageBucket: "crown-clothing-db-42ce3.appspot.com",
-    messagingSenderId: "994507383250",
-    appId: "1:994507383250:web:7899ecfb38992291eba80b",
-    measurementId: "G-44GJHBGZ5L"
-  };
+  apiKey: "AIzaSyDm3JMHGSu8-hpDLE4OSQFgzEnFMcjE5A4",
+  authDomain: "crown-clothing-db-42ce3.firebaseapp.com",
+  databaseURL: "https://crown-clothing-db-42ce3.firebaseio.com",
+  projectId: "crown-clothing-db-42ce3",
+  storageBucket: "crown-clothing-db-42ce3.appspot.com",
+  messagingSenderId: "994507383250",
+  appId: "1:994507383250:web:7899ecfb38992291eba80b",
+  measurementId: "G-44GJHBGZ5L"
+};
 
+  export const createUserProfileDocument = async (userAuth, additionalData ) => {
+    console.log(userAuth);
+    if (!userAuth) {
+      console.log('no user auth');
+      return;
+    }
+
+    const userRef = firestore.doc(`/users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date();
+
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        })
+
+      }
+      catch (error) {
+        console.log('error creating user', error.message)
+      }
+    }
+
+    return userRef;
+  }
 firebase.initializeApp(firebaseConfig);
 
 export const auth = firebase.auth();
