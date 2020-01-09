5fc3c00023b62ffc9aa29be11eb44d8d2387550d
ldail <leondailofficial@gmail.com>
2020-01-08 18:08:59 -0800

    header and shop page added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import {Switch, Route} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component'
+
+import './App.css';
 
 class App extends Component {
   render() {
-    return <HomePage />
+    return (
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+        </Switch>
+      </div>
+    )
   }
 }
 
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -6,40 +6,41 @@ class Directory extends Component {
     constructor() {
         super();
         this.state = {
-            sections : [
+            sections: [
                 {
-                  title: 'hats',
-                  imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                  id: 1,
-                  linkUrl: 'shop/hats'
+                    title: 'hats',
+                    imageUrl: 'images/hats.png', // local file in public/images/
+                    id: 1,
+                    linkUrl: 'shop/hats'
+            
                 },
                 {
-                  title: 'jackets',
-                  imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                  id: 2,
-                  linkUrl: 'shop/jackets'
+                    title: 'jackets',
+                    imageUrl: 'images/jackets.png', // local file in public/images/
+                    id: 2,
+                    linkUrl: 'shop/jackets'
                 },
                 {
-                  title: 'sneakers',
-                  imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                  id: 3,
-                  linkUrl: 'shop/sneakers'
+                    title: 'sneakers',
+                    imageUrl: 'images/sneakers.png', // local file in public/images/
+                    id: 3,
+                    linkUrl: 'shop/sneakers'
                 },
                 {
-                  title: 'womens',
-                  imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-                  size: 'large',
-                  id: 4,
-                  linkUrl: 'shop/womens'
+                    title: 'womens',
+                    imageUrl: 'images/womens.png', // local file in public/images/
+                    size: 'large',
+                    id: 4,
+                    linkUrl: 'shop/womens'
                 },
                 {
-                  title: 'mens',
-                  imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-                  size: 'large',
-                  id: 5,
-                  linkUrl: 'shop/mens'
+                    title: 'mens',
+                    imageUrl: 'images/men.png', // local file in public/images/
+                    size: 'large',
+                    id: 5,
+                    linkUrl: 'shop/mens'
                 }
-              ]
+            ]
               
         }
     }
